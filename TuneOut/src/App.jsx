f248f6174fb85176624558b6a40dd9f26d18fa43
rivka-flig <rivkaflig@gmail.com>
2025-06-25 19:15:04 -0400

    Moved home page to pages

--- TuneOut/src/App.jsx
+++ TuneOut/src/App.jsx
@@ -1,18 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Home from './Home.jsx';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import SiteHeader from './components/SiteHeader';
+import Home from './pages/Home';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Playlists from './pages/Playlists';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <Home/>
-    </>
-  )
+    <Router>
+      <SiteHeader />
+      <div className="container mt-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/playlists" element={<Playlists />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
-export default App
+export default App;
