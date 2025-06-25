a8fa96ab65f7c49b1bb07ade00dea0cc66202eb2
rivka-flig <rivkaflig@gmail.com>
2025-06-25 17:54:23 -0400

    Hopefully fixed routing error

--- TuneOut/src/App.jsx
+++ TuneOut/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
 import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Playlists from './pages/Playlists';
+// import About from './pages/About';
+// import Contact from './pages/Contact';
+// import Playlists from './pages/Playlists';
 import './App.css';
 
 function App() {
@@ -13,9 +13,9 @@ function App() {
       <div className="container mt-4">
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/playlists" element={<Playlists />} />
+          {/* <Route path="/about" element={<About />} /> */}
+          {/* <Route path="/contact" element={<Contact />} /> */}
+          {/* <Route path="/playlists" element={<Playlists />} /> */}
         </Routes>
       </div>
     </Router>
