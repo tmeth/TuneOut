c1dba70b834a2ab9d9c3c8362f6f98b2767f60b9
leahfeldmanlf@gmail.com <leahfeldmanlf@gmail.com>
2025-06-25 21:06:45 -0400

    contact us branch

--- TuneOut/src/App.jsx
+++ TuneOut/src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import SiteHeader from './components/SiteHeader';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
-import Playlists from './pages/Playlists';
+import Home from './Home';
+import About from './About';
+import Contact from './Contact.jsx';
+import Playlists from './Playlists';
 import './App.css';
 
 function App() {
