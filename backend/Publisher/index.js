ac9961f9bbcb433d0940d3506eb07504079376b6
tmeth <methtoby@gmail.com>
2025-08-10 12:37:09 -0400

    changes-personal-touch

--- backend/Publisher/index.js
+++ backend/Publisher/index.js
@@ -18,7 +18,7 @@ exports.handler = async () => {
     message = `🎶 No new playlists—why not make one?`;
   }
 
-  message += `\n\n👉 Click here to view or add playlists: http://localhost:5173/`;
+  message += `\n\n👉 Click here to view or add playlists: https://main.d37vhm5bmnvd3f.amplifyapp.com`;
 
   await sns.publish({
     Message: message,
